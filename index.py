a7334f08351672b2247a3c5a3cf349857e55d08a
Daniel RICKLIN <ricklin.daniel@outlook.fr>
2024-09-29 17:31:56 +0200

    flake8 and black

--- index.py
+++ index.py
@@ -5,23 +5,43 @@ import logging
 from src.FortigateApi import FortiGateApi
 from src.FortigateSSH import FortigateSSH
 
-logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
+logging.basicConfig(format="%(asctime)s %(levelname)s: %(message)s")
 logging.getLogger().setLevel(logging.INFO)
 
 # def list_of_config(arg):
 #     return arg.split(',')
 
 parser = argparse.ArgumentParser(description="Make nice Excel Fortigate Report")
-parser.add_argument('--ip', required=True, help="set the IP you want to access")
-parser.add_argument('--args-mode', action="store_true", help="to run the program only with arguments")
-parser.add_argument('--ssh', action="store_true", help="if you want to request your Fortigate through SSH")
-parser.add_argument('--bastion', action="store_true", help="Only for SSH if you need to access you Fortigate through a bastion")
-parser.add_argument('--api', action="store_true", help="if you want to request your Fortigate through the API")
-parser.add_argument('--api-key', help="set the API KEY")
-parser.add_argument('--proxy-port', help="set the proxy port is you need to access you Fortigate through a proxy")
-parser.add_argument('--override-filename', help="set the name you want")
+parser.add_argument("--ip", required=True, help="set the IP you want to access")
+parser.add_argument("--port", help="set the port you want to access", default=443)
+parser.add_argument(
+    "--args-mode", action="store_true", help="to run the program only with arguments"
+)
+parser.add_argument(
+    "--ssh",
+    action="store_true",
+    help="if you want to request your Fortigate through SSH",
+)
+parser.add_argument(
+    "--bastion",
+    action="store_true",
+    help="Only for SSH if you need to access you Fortigate through a bastion",
+)
+parser.add_argument(
+    "--api",
+    action="store_true",
+    help="if you want to request your Fortigate through the API",
+)
+parser.add_argument("--api-key", help="set the API KEY")
+parser.add_argument(
+    "--proxy-port",
+    help="set the proxy port is you need to access you Fortigate through a proxy",
+)
+parser.add_argument("--override-filename", help="set the name you want")
 # parser.add_argument('--config-list', help="set the list of configs you want in you report", type=list_of_config)
-parser.add_argument('--verbose', action="store_true", help="Verbose logging mode, will log all actions")
+parser.add_argument(
+    "--verbose", action="store_true", help="Verbose logging mode, will log all actions"
+)
 args = parser.parse_args()
 
 if args.verbose:
@@ -29,18 +49,24 @@ if args.verbose:
     logging.debug(args)
 
 if args.api and args.ssh:
-    logging.error(f"--api and --ssh are both set")
+    logging.error("--api and --ssh are both set")
     exit(1)
     # parser.error("You need to choose between SSH and API...")
 elif args.api is None and args.ssh is None:
-    logging.error(f"--api and --ssh are not set")
+    logging.error("--api and --ssh are not set")
     exit(1)
     # parser.error("You need to choose between SSH and API...")
 
 if args.args_mode:
     if args.api:
         logging.info("Running program through API")
-        forti = FortiGateApi(ip=args.ip, api_key=args.api_key, proxy_local_port=args.proxy_port, filename=args.override_filename)
+        forti = FortiGateApi(
+            ip=args.ip,
+            port=args.port,
+            api_key=args.api_key,
+            proxy_local_port=args.proxy_port,
+            filename=args.override_filename,
+        )
         forti.get_xlsx_file()
 else:
     ip = input("IP : ")
@@ -55,11 +81,29 @@ else:
             while q not in ["Y", "N"]:
                 q = input("L'utilisateur est le même ? (Y/N) : ")
                 if q == "Y":
-                    forti = FortigateSSH(ip=ip, port=port, user=user, password=password, bastion_port=bastion_port, bastion_ip=bastion_ip, bastion_user=user, bastion_password=password)
+                    forti = FortigateSSH(
+                        ip=ip,
+                        port=port,
+                        user=user,
+                        password=password,
+                        bastion_port=bastion_port,
+                        bastion_ip=bastion_ip,
+                        bastion_user=user,
+                        bastion_password=password,
+                    )
                 elif q == "N":
                     bastion_user = input("Nom d'utilisateur Bastion: ")
                     bastion_password = getpass("Mot de passe Bastion: ")
-                    forti = FortigateSSH(ip=ip, port=port, user=user, password=password, bastion_port=bastion_port, bastion_ip=bastion_ip, bastion_user=bastion_user, bastion_password=bastion_password)
+                    forti = FortigateSSH(
+                        ip=ip,
+                        port=port,
+                        user=user,
+                        password=password,
+                        bastion_port=bastion_port,
+                        bastion_ip=bastion_ip,
+                        bastion_user=bastion_user,
+                        bastion_password=bastion_password,
+                    )
         else:
             forti = FortigateSSH(ip=ip, port=port, user=user, password=password)
             # forti.get_xlsx_file()
@@ -67,7 +111,9 @@ else:
         api_key = input("Clé d'API : ")
         proxy_port = input("Port proxy : ")
         filename = input("Override filename : ")
-        forti = FortiGateApi(ip=ip, api_key=api_key, proxy_local_port=proxy_port, filename=filename)
+        forti = FortiGateApi(
+            ip=ip, api_key=api_key, proxy_local_port=proxy_port, filename=filename
+        )
         forti.get_xlsx_file()
     else:
         print("Il faut ajouter l'option --ssh ou --api pour avancer")
--- src/FortigateApi.py
+++ src/FortigateApi.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
 import requests
-from .FortigateToXlsxFromAPI import FortigateToXlsxFromAPI
-requests.packages.urllib3.disable_warnings()
 import logging
+from .FortigateToXlsxFromAPI import FortigateToXlsxFromAPI
 
-class FortiGateApi():
+requests.packages.urllib3.disable_warnings()
+
+
+class FortiGateApi:
     """Get fortigate configuration by API"""
 
     ENDPOINT_FIREWALL_VDOM = ["/system/vdom", "vdom"]
@@ -19,19 +21,30 @@ class FortiGateApi():
     ENDPOINT_FIREWALL_IPPOOL = ["/firewall/ippool", "ippool"]
     ENDPOINT_FIREWALL_POLICY = ["/firewall/policy", "policy"]
 
-    def __init__(self, ip: str = "", port: int = 40403, api_key: str = "", proxy_local_port: int = 0, filename: str = "") -> None:
+    def __init__(
+        self,
+        ip: str = "",
+        port: int = 443,
+        api_key: str = "",
+        proxy_local_port: int = 0,
+        filename: str = "",
+    ) -> None:
         self.ip = ip
         self.port = port
         self.base_url = f"https://{ip}:{port}/api/v2/cmdb"
         self.api_key = api_key
         self.filename = filename
         if proxy_local_port:
-            self.proxy = dict(https=f'socks5://@127.0.0.1:{proxy_local_port}') if proxy_local_port != 0 else dict()
+            self.proxy = (
+                dict(https=f"socks5://@127.0.0.1:{proxy_local_port}")
+                if proxy_local_port != 0
+                else dict()
+            )
         else:
             self.proxy = 0
         self.headers = {
             "Accept": "application/json",
-            "Authorization": f"Bearer {api_key}"
+            "Authorization": f"Bearer {api_key}",
         }
         self.vdom = self.get_vdoms()
         self.configuration = []
@@ -39,21 +52,41 @@ class FortiGateApi():
     def get_vdoms(self) -> list[dict]:
         logging.debug("Requesting vdoms list")
         if self.proxy:
-            vdoms = requests.get(f"{self.base_url}{FortiGateApi.ENDPOINT_FIREWALL_VDOM[0]}?access_token={self.api_key}", headers=self.headers, verify=False, proxies=self.proxy)
+            vdoms = requests.get(
+                f"{self.base_url}{FortiGateApi.ENDPOINT_FIREWALL_VDOM[0]}?access_token={self.api_key}",
+                headers=self.headers,
+                verify=False,
+                proxies=self.proxy,
+            )
         else:
-            vdoms = requests.get(f"{self.base_url}{FortiGateApi.ENDPOINT_FIREWALL_VDOM[0]}?access_token={self.api_key}", headers=self.headers, verify=False)
-        logging.debug(vdoms.json()['results'])
-        return vdoms.json()['results']
+            vdoms = requests.get(
+                f"{self.base_url}{FortiGateApi.ENDPOINT_FIREWALL_VDOM[0]}?access_token={self.api_key}",
+                headers=self.headers,
+                verify=False,
+            )
+        logging.debug(vdoms.json()["results"])
+        return vdoms.json()["results"]
 
     def get_api_request(self, endpoint) -> dict:
         self.configuration = self.vdom
         for index, vdom in enumerate(self.configuration):
-            logging.debug(f"For VDOM {vdom['name']}, Requesting {endpoint[1]}, Endpoint : {endpoint[0]}")
+            logging.debug(
+                f"For VDOM {vdom['name']}, Requesting {endpoint[1]}, Endpoint : {endpoint[0]}"
+            )
             if self.proxy:
-                req = requests.get(f"{self.base_url}{endpoint[0]}?vdom={vdom['name']}&access_token={self.api_key}", headers=self.headers, verify=False, proxies=self.proxy)
+                req = requests.get(
+                    f"{self.base_url}{endpoint[0]}?vdom={vdom['name']}&access_token={self.api_key}",
+                    headers=self.headers,
+                    verify=False,
+                    proxies=self.proxy,
+                )
             else:
-                req = requests.get(f"{self.base_url}{endpoint[0]}?vdom={vdom['name']}&access_token={self.api_key}", headers=self.headers, verify=False)
-            self.configuration[index][endpoint[1]] = req.json()['results']
+                req = requests.get(
+                    f"{self.base_url}{endpoint[0]}?vdom={vdom['name']}&access_token={self.api_key}",
+                    headers=self.headers,
+                    verify=False,
+                )
+            self.configuration[index][endpoint[1]] = req.json()["results"]
         return self.configuration
 
     def get_xlsx_file(self):
